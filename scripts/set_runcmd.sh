1bd735ea0584e99df435a439cc4174a336e3d665
Blaze <andrewblue1988@gmail.com>
2021-12-01 20:23:43 +0800

    Create set_runcmd.sh

---
+++ scripts/set_runcmd.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Process Args
+while getopts g:v: flag;
+do
+    case "${flag}" in
+        g) RG_NAME=${OPTARG};;
+        v) VMSS=${OPTARG};;
+    esac
+done
+
+echo "Target VMSS: ${VMSS}"
+echo "in resource group ${RG_NAME}"
+echo "============================="
+
+# Renew runCommand on instances when necessary
+for i in $(az vmss list-instances -g $RG_NAME --name $VMSS -o tsv | awk '{print $4}' | awk -F '/' '{print $NF}');do
+        echo "On instance $i"
+        az vmss get-instance-view -g MC_RG-EASTUS_LAB-AKS-CNI_EASTUS --name aks-system1-21863241-vmss --instance-id $i --query vmAgent.extensionHandlers | grep RunCommandLinux
+        if [[ $? -eq 0 ]]; then
+                echo "Instance $i currently has runCommand extension. Refreshing the extension."
+                az vmss run-command invoke -g MC_RG-EASTUS_LAB-AKS-CNI_EASTUS --name aks-system1-21863241-vmss --command-id RemoveRunCommandLinuxExtension --instance-id $i
+        else
+                echo "Instance $i does not have runCommand extension installed. No need to refresh."
+        fi
+done
