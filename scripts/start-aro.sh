739a2dc542da2b14eab65cc04d3b397db283ac56
andrewblue1988@gmail.com <andrewblue1988@gmail.com>
2023-06-12 16:43:09 +0800

    Update start-aro.sh

--- scripts/start-aro.sh
+++ scripts/start-aro.sh
@@ -15,7 +15,7 @@ az network vnet subnet update -n subnet1 --vnet-name aro-vnet -g rg-centralindia
 az network vnet subnet create -n aro-master --vnet-name aro-vnet -g rg-centralindia --address-prefixes 10.15.208.0/24 --service-endpoints Microsoft.ContainerRegistry --disable-private-link-service-network-policies true
 # Follow https://docs.microsoft.com/en-us/azure/openshift/howto-add-update-pull-secret#prepare-your-pull-secret to get reg pull secrets from Red Hat OpenShift cluster manager portal
 if [ -f pull-secret.txt ]; then
-	az aro create --master-subnet aro-master -n aro-mock-38934 -g rg-centralindia --worker-subnet subnet1 --vnet aro-vnet --pull-secret @pull-secret.txt --worker-count 3 --master-vm-size Standard_D8s_v3 --worker-vm-size Standard_D4as_v4 --client-id $CLIENT_ID --client-secret $CLIENT_SECRET
+	az aro create --master-subnet aro-master -n aro-mock-38934 -g rg-centralindia --worker-subnet subnet1 --vnet aro-vnet --pull-secret @pull-secret.txt --worker-count 3 --master-vm-size Standard_D8s_v3 --worker-vm-size Standard_D4as_v4 --client-id $CLIENT_ID --client-secret $CLIENT_SECRET --version 4.11.26
 else 
-	az aro create --master-subnet aro-master -n aro-mock-38934 -g rg-centralindia --worker-subnet subnet1 --vnet aro-vnet --worker-count 3 --master-vm-size Standard_D8s_v3 --worker-vm-size Standard_D4as_v4 --client-id $CLIENT_ID --client-secret $CLIENT_SECRET
+	az aro create --master-subnet aro-master -n aro-mock-38934 -g rg-centralindia --worker-subnet subnet1 --vnet aro-vnet --worker-count 3 --master-vm-size Standard_D8s_v3 --worker-vm-size Standard_D4as_v4 --client-id $CLIENT_ID --client-secret $CLIENT_SECRET --version 4.11.26
 fi	
