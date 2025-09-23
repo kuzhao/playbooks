20d662bb07b4135626582e9bc54ced1b309683ed
andrewblue1988@gmail.com <andrewblue1988@gmail.com>
2025-09-23 16:31:25 -0400

    Add script to list containers and memory-intensive PIDs

---
+++ scripts/k8s/crictl-ctn-highmem.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+################
+#Dependency:
+#  crictl,pstree
+#  containerd service up and running
+#Code logic:
+# Output a list of all containers plus their pod names and pids
+# Output top memory consuming PIDs
+################
+
+crictl ps -o json | jq -r '.containers[] | [.id, .labels."io.kubernetes.pod.name", .metadata.name] | @tsv' | while read -r container_id pod_name container_name; do pid=$(crictl inspect "$container_id" | jq -r '.info.pid'); echo "Container: $container_name, Pod: $pod_name, PID: $pid"; done
+ps -eo pid,ppid,user,%mem,%cpu,rss,stat,start,time,comm --sort=-%mem | head -n 6 | awk 'NR>1 {print $1}' | while read pid; do echo "Memory-intensive Process PID: $pid";pstree -pTs "$pid";done
